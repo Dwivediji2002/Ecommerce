8d773fea62a0012bc88b103461ec73bb781a6fcb
priyam dwivedi <priyamdwivedi107@gmail.com>
2023-11-25 03:24:45 +0530

    .env-added

---
+++ storage/framework/views/d3668e7d264aa9d67d307075bb44c324.php
@@ -0,0 +1,10 @@
+
+
+<?php $__env->startSection('content'); ?>
+    <div class="card">
+        <div class = "card-body">
+            <h1>Priyam Dwivedi</h1>
+        </div>
+    </div>
+<?php $__env->stopSection(); ?>
+<?php echo $__env->make('layouts.admin', \Illuminate\Support\Arr::except(get_defined_vars(), ['__data', '__path']))->render(); ?><?php /**PATH D:\xampp\htdocs\PRIYAM\ecommerce\resources\views/admin/index.blade.php ENDPATH**/ ?>
